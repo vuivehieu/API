a478d277bf196697b1b40626c832bf49f7c93c28
Quocviet2000 <84843130+Quocviet2000@users.noreply.github.com>
2022-06-16 22:40:40 +0700

    them tips controller

---
+++ Project3-jamesthew/Controllers/TipsController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace Project3_jamesthew.Controllers
+{
+    public class TipsController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
